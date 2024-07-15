ee26fd832d5a67c94dee18c8acb891152433ff5a
Patrick Zandl <patrick.zandl@marigold.cz>
2024-07-15 13:43:41 +0200

    Create process_thumbnails.sh

---
+++ process_thumbnails.sh
@@ -0,0 +1,7 @@
+   #!/bin/bash
+   
+   # Instalace potřebných nástrojů
+   pip install Pillow requests cloudinary
+
+   # Spuštění Python skriptu pro úpravu thumbnailů
+   python process_thumbnails.py
